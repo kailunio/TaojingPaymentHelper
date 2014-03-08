675e5e4a5b5d60bcd3641be04aa94234e344bb9e
Real Xu <xukailun@hotmail.com>
2014-03-08 23:46:41 +0800

    修复数据中出现Unix换行符造成的Bug

---
+++ TaojingPaymentHelper/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TaojingPaymentHelper {
+    public static class Util {
+        public static string[] ReadAllLines(string path, Encoding encoding) {
+            string content = File.ReadAllText(path, encoding);
+            string[] lines = content.Split(new string[]{"\r\n"}, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < lines.Length; i++) {
+                if (lines[i].Contains('\r')) {
+                    lines[i] = lines[i].Replace("\r", "");
+                }
+            }
+            return lines;
+        }
+    }
+}
